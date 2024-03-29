6b762d93f52c7d9f06489170b747cf7f0d05201e
Suyog Patil <suyog0057@gmail.com>
2024-03-29 18:27:36 +0000

    project init

---
+++ service/stock_detail_service.js
@@ -0,0 +1,10 @@
+// fetch to this url 
+// "https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/tsla/quote"
+
+export default class StockPriceService {
+    constructor(){}
+
+    fetchStock() {
+        // TODO : write the fetch logic
+    }
+}
