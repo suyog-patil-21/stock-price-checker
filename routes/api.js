928e26755a6cfe689654572a44e71b687952833a
Suyog Patil <suyog0057@gmail.com>
2024-04-04 16:43:57 +0000

    accepting request inputs

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,19 @@
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async function (req, res){
+      const stock = req.query.stock;
+      let like;
+      if(req.query.like === undefined){
+        like = false;
+      }
+      const userIpAddress = req.ip;
+      const isStockArrayReq = Array.isArray(stock);
+      console.log(`ip:${userIpAddress}\n,stock: ${stock}\n,like:${like}`);
+      if(isStockArrayReq){
+        // TODO : for multuple stock
+      }
+      // TODO : for single stock
     });
     
 };
