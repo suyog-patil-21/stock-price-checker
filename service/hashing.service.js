bfdc389a0d6b91b292f61e6435725efbdd157da9
Suyog Patil <suyog0057@gmail.com>
2024-04-26 17:52:25 +0000

    add: hashing class & implementation is incomplete

--- service/hashing.service.js
+++ service/hashing.service.js
@@ -1,6 +1,26 @@
-module.exports = class HashingService{
+const bcrypt = require('bcrypt');
 
-   async getHash(data){
+const SALTROUNDS = 12;
 
-   }
+module.exports = class HashingService {
+
+    static async getHash(plainData) {
+        try {
+            const salt = await bcrypt.genSalt(SALTROUNDS);
+            const hash = await bcrypt.hash(plainData, salt);
+            return hash;
+        }
+        catch (err) {
+            console.log(`Error in HashingService.getHash(): \n${err}`);
+        }
+    }
+
+    static async compareHash(hash,plainData){
+        try {
+            return bcrypt.compare(plainData,hash);
+        }
+        catch (err) {
+            console.log(`Error in HashingService.compareHash(): \n${err}`);
+        }   
+    }
 }
