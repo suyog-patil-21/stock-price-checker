5761ef21aeca505e7f75c4036003394051542dc1
Suyog Patil <suyog0057@gmail.com>
2024-04-26 17:53:21 +0000

    chore: implementation is incomplete

--- service/user-like-info.service.js
+++ service/user-like-info.service.js
@@ -1,14 +1,16 @@
 const UserLikeInfoDAO = require('../data/user-like-info.data');
+const HashingService = require("./hashing.service");
 
 module.exports = class UserLikeInfoService {
     constructor() {
         this._userLikeInfoDAO = new UserLikeInfoDAO();
     }
 
-    async createUserLikeInfo(symbol, ipHash) {
+    async createUserLikeInfo(symbol, ipAddress) {
         try {
+            // FIXME: logic of the hash is wrong 
+            const ipHash = await HashingService.getHash(ipAddress);
             const result = await this._userLikeInfoDAO.getUserLikeInfoDataBySymbolAndHash(symbol, ipHash);
-            console.log(result);
             if (result === null) {
                 return await this._userLikeInfoDAO.createUserLikeInfo(symbol, ipHash);
             }
