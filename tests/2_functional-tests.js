dead196f9c3b845b00ba1a7d27d9e187d1780c0c
Suyog Patil <suyog0057@gmail.com>
2024-04-29 15:09:43 +0000

    fix: Functional Testing Done & Project All test Pass

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,15 +7,16 @@ const UserLikeInfoService = require('../service/user-like-info.service')
 chai.use(chaiHttp);
 
 suite('Functional Tests', function () {
+    const _userLikeInfoService = new UserLikeInfoService();
 
-    this.afterEach(async function () {
-        const response = chai.request(server).get('/api/stock-prices/').query({ stock: 'GOOG' });
-        const _userLikeInfoService = new UserLikeInfoService();
-        await _userLikeInfoService.deleteAllUserLikeInfoByIpHash(response.req.ip);
+    after(async function () {
+        await _userLikeInfoService.deleteAllUserLikeInfoCollection();
     });
 
-    it('Viewing one stock: GET request to /api/stock-prices/', function () {
+
+    test('Viewing one stock: GET request to /api/stock-prices/', function () {
         chai.request(server).get('/api/stock-prices/').query({ stock: 'GOOG' }).end((err, res) => {
+            chai.expect(res.status).to.be.equal(200);
             chai.expect(res.body).to.have.property('stockData');
             chai.expect(res.body.stockData).to.have.property('stock');
             chai.expect(res.body.stockData).to.have.property('price');
@@ -26,11 +27,10 @@ suite('Functional Tests', function () {
         });
     });
 
-    it('Viewing one stock and liking it: GET request to /api/stock-prices/', async function () {
-        const response = await chai.request(server).get('/api/stock-prices/').query({ stock: 'GOOG' });
-        const likes = response.body.stockData.likes;
-
+    test('Viewing one stock and liking it: GET request to /api/stock-prices/', async function () {
+        const initialLikes = await _userLikeInfoService.getStockLikeCount('GOOG');
         const res = await chai.request(server).get('/api/stock-prices/').query({ stock: 'GOOG', like: true });
+        chai.expect(res.status).to.be.equal(200);
         chai.expect(res.body).to.have.property('stockData');
         chai.expect(res.body.stockData).to.have.property('stock');
         chai.expect(res.body.stockData).to.have.property('price');
@@ -38,15 +38,13 @@ suite('Functional Tests', function () {
         assert.isNumber(res.body.stockData.price);
         assert.isNumber(res.body.stockData.likes);
         chai.expect(res.body.stockData.stock).to.be.equal('GOOG');
-        chai.expect(res.body.stockData.likes).to.be.equal(likes + 1);
-        return;
+        chai.expect(res.body.stockData.likes).to.be.equal((initialLikes + 1));
     });
 
-    it('Viewing the same stock and liking it again: GET request to /api/stock-prices/', async function () {
-        const response = await chai.request(server).get('/api/stock-prices/').query({ stock: 'GOOG' });
-        const initialLikes = response.body.stockData.likes;
-
+    test('Viewing the same stock and liking it again: GET request to /api/stock-prices/', async function () {
+        const initialLikes = await _userLikeInfoService.getStockLikeCount('GOOG');
         const res = await chai.request(server).get('/api/stock-prices/').query({ stock: 'GOOG', like: true });
+        chai.expect(res.status).to.be.equal(200);
         chai.expect(res.body).to.have.property('stockData');
         chai.expect(res.body.stockData).to.have.property('stock');
         chai.expect(res.body.stockData).to.have.property('price');
@@ -57,8 +55,9 @@ suite('Functional Tests', function () {
         chai.expect(res.body.stockData.likes).to.be.equal(initialLikes);
     });
 
-    it('Viewing two stocks: GET request to /api/stock-prices/', function () {
-        chai.request(server).get('/api/stock-prices/').query({ stock: ['GOOG', 'TSLA'] }).end((err, res) => {
+    test('Viewing two stocks: GET request to /api/stock-prices/', function () {
+        chai.request(server).get('/api/stock-prices/').query({ stock: ['MSFT', 'TSLA'] }).end((err, res) => {
+            chai.expect(res.status).to.be.equal(200);
             chai.expect(res.body).to.have.property('stockData');
             assert.isArray(res.body.stockData);
             chai.expect(res.body.stockData.length).to.be.equal(2);
@@ -68,7 +67,7 @@ suite('Functional Tests', function () {
             chai.expect(res.body.stockData[0]).to.have.property('rel_likes');
             assert.isNumber(res.body.stockData[0].price);
             assert.isNumber(res.body.stockData[0].rel_likes);
-            chai.expect(res.body.stockData[0].stock).to.be.equal('GOOG');
+            chai.expect(res.body.stockData[0].stock).to.be.equal('MSFT');
 
             chai.expect(res.body.stockData[1]).to.have.property('stock');
             chai.expect(res.body.stockData[1]).to.have.property('price');
@@ -79,8 +78,12 @@ suite('Functional Tests', function () {
         });
     });
 
-    it('Viewing two stocks and liking them: GET request to /api/stock-prices/', async function () {
-        const res = await chai.request(server).get('/api/stock-prices/').query({ stock: ['GOOG', 'TSLA'], like: true });
+    test('Viewing two stocks and liking them: GET request to /api/stock-prices/', async function () {
+        const initialStock0Likes = await _userLikeInfoService.getStockLikeCount('MSFT');
+        const initialStock1Likes = await _userLikeInfoService.getStockLikeCount('TSLA');
+
+        const res = await chai.request(server).get('/api/stock-prices/').query({ stock: ['MSFT', 'TSLA'], like: true });
+        chai.expect(res.status).to.be.equal(200);
         chai.expect(res.body).to.have.property('stockData');
         assert.isArray(res.body.stockData);
         chai.expect(res.body.stockData.length).to.be.equal(2);
@@ -90,7 +93,8 @@ suite('Functional Tests', function () {
         chai.expect(res.body.stockData[0]).to.have.property('rel_likes');
         assert.isNumber(res.body.stockData[0].price);
         assert.isNumber(res.body.stockData[0].rel_likes);
-        chai.expect(res.body.stockData[0].stock).to.be.equal('GOOG');
+        chai.expect(res.body.stockData[0].stock).to.be.equal('MSFT');
+        chai.expect(res.body.stockData[0].rel_likes).to.be.equal(initialStock0Likes - initialStock1Likes);
 
         chai.expect(res.body.stockData[1]).to.have.property('stock');
         chai.expect(res.body.stockData[1]).to.have.property('price');
@@ -98,6 +102,7 @@ suite('Functional Tests', function () {
         assert.isNumber(res.body.stockData[1].price);
         assert.isNumber(res.body.stockData[1].rel_likes);
         chai.expect(res.body.stockData[1].stock).to.be.equal('TSLA');
+        chai.expect(res.body.stockData[1].rel_likes).to.be.equal(initialStock1Likes - initialStock0Likes);
     });
 
 });
